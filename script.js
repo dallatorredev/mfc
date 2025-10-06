f8213e31efa2365bfdd844357383c005c13031ca
dallatorredev <dallatorre.dev@gmail.com>
2025-10-06 23:04:05 +0100

    Meu primeiro commit

---
+++ script.js
@@ -0,0 +1,10 @@
+function btnLogin() {
+
+    event.preventDefault();
+
+    const user = document.getElementById('user').value;
+    const pass = document.getElementById('pass').value;
+
+    console.log(user);
+    console.log(pass);
+}
